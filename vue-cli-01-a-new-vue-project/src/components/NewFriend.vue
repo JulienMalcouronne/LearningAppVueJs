f127524e8700665dec3ab3cce42d42e9d990df3d
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-03-31 22:09:54 +0200

    init

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <div>
+      <label for="">Name</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="">Phone</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="">E-mail</label>
+      <input type="text" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
